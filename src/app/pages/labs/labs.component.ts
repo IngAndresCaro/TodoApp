912fb0eb65ef7d04cd0ee327ea0b978036e1dd7a
Andres Caro <andres.caro@sofka.com.co>
2024-02-22 13:12:21 -0500

    Controlando un input

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
@@ -42,6 +43,12 @@ export class LabsComponent {
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   });
 
+  colorCtrl = new FormControl()
+
+  constructor(){
+    this.colorCtrl.valueChanges.subscribe(value => console.log(value))
+  }
+
   clickHandler() {
     alert('Papitas');
   }
@@ -67,6 +74,14 @@ export class LabsComponent {
     const newValue = input.value;
     this.personSignal.update((prevState) => {
       return { ...prevState, age: parseInt(newValue, 10) };
+    });
+  }
+  
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.personSignal.update((prevState) => {
+      return { ...prevState, name: newValue };
     });
   }
 }
